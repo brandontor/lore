3a3eb9629aec79e8307e75064c152c4db81b3865
Brandon Tor <brandon.tor@hotmail.com>
2023-05-26 15:09:15 -0400

    cleaned out defaults and added colors to ts config

---
+++ components/sidebar-nav.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+function SideBarNav({}: Props) {
+  return <div>sidebar-nav</div>;
+}
+
+export default SideBarNav;
---
+++ components/topbar-nav.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+function TopBarNav({}: Props) {
+  return <div>topbar-nav</div>;
+}
+
+export default TopBarNav;
