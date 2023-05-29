f2eb47ca74f13dcfc2be3f288168948b78125d62
Brandon Tor <brandon.tor@hotmail.com>
2023-05-28 20:47:29 -0400

    added profile dropdown menu

--- components/rootlayout/topbar-nav.tsx
+++ components/rootlayout/topbar-nav.tsx
@@ -1,15 +1,80 @@
 import React from "react";
+import { CreditCard, LogOut, PlusCircle, Settings, User } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 type Props = {};
+
+const ProfileMenu = () => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="flex items-center m-8 outline-none">
+        <Button
+          variant="ghost"
+          className="relative h-8 w-8 rounded-full mr-4 outline-none"
+        >
+          <Avatar className="h-8 w-8">
+            <AvatarImage src="/avatars/01.png" alt="@shadcn" />
+            <AvatarFallback>SC</AvatarFallback>
+          </Avatar>
+        </Button>
+        <h3>Brandon Tor</h3>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">shadcn</p>
+            <p className="text-xs leading-none text-nord5">m@example.com</p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <User className="mr-2 h-4 w-4" />
+            <span>Profile</span>
+            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <CreditCard className="mr-2 h-4 w-4" />
+            <span>Billing</span>
+            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Settings className="mr-2 h-4 w-4" />
+            <span>Settings</span>
+            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <PlusCircle className="mr-2 h-4 w-4" />
+            <span>New Team</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Log out</span>
+          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
 
 function TopBarNav({}: Props) {
   return (
     <header className="w-full h-[5%] flex items-center justify-between border-b-[1px] border-nord3">
       <p className="pl-10 text-nord6">Lore</p>
-      <div className="flex pr-10">
-        <p>Picture</p>
-        <p>Brandon</p>
-      </div>
+      <ProfileMenu />
     </header>
   );
 }
