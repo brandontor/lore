974920658d8249f39caf4465f3721b666d1e6d8f
Brandon Tor <brandon.tor@hotmail.com>
2023-05-29 17:26:40 -0400

    added tabs working on inserting card content next

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,33 @@
 import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 type Props = {};
 
+const CurrentContent = () => {
+  return <div>Hello Current</div>;
+};
+
+const PreviousContent = () => {
+  return <div>Hello Previous</div>;
+};
+
 function IndexPage({}: Props) {
-  return <div>Home</div>;
+  return (
+    <main className="container mx-auto py-8">
+      <Tabs defaultValue="current" className="w-[400 px]">
+        <TabsList>
+          <TabsTrigger value="current">Current</TabsTrigger>
+          <TabsTrigger value="previous">Previous</TabsTrigger>
+        </TabsList>
+        <TabsContent value="current">
+          <CurrentContent />
+        </TabsContent>
+        <TabsContent value="previous">
+          <PreviousContent />
+        </TabsContent>
+      </Tabs>
+    </main>
+  );
 }
 
 export default IndexPage;
